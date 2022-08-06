aab5f0a3537ea74d936327a85946c40a232716d5
Quinn Torres <quinntorrs@gmail.com>
2022-08-06 13:58:04 -0400

    Move reset file into new css folder

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'reset.scss';
+import 'css/reset.scss';
 import App from 'components/App';
 
 const root = ReactDOM.createRoot(
