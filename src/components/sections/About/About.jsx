aff78550bdf272b99e2b7e771844f7b6729998ba
Quinn Torres <quinntorrs@gmail.com>
2022-08-21 12:32:15 -0400

    Add about section

--- src/components/sections/About/About.jsx
+++ src/components/sections/About/About.jsx
@@ -3,11 +3,24 @@ import 'components/sections/About/About.scss';
 import ProfilePicture from 'img/profile.jpg';
 
 import SectionHeader from 'components/sections/SectionHeader/SectionHeader';
+import TextLink from 'components/navigation/TextLink/TextLink';
 
 function About() {
   return (
     <div className="About">
-      <SectionHeader img={ProfilePicture} imgAlt="Profile" title="About Me" anchor="about-me"/>
+      <SectionHeader img={ProfilePicture} imgAlt="Profile" title="Hello!" anchor="about-me">
+        <p>
+        I'm a senior front-end developer at <TextLink href="https://www.dotdashmeredith.com/">Dotdash Meredith</TextLink>, the largest digital publisher in the US. I have 3 years experience developing performance-first sites, which are among the <TextLink href="https://webperf.xyz">fastest on the web</TextLink>. I also have experience with:
+        </p>
+        <ul>
+          <li>⚛️ React.js (this site)</li>
+          <li>✅ Vue.js</li>
+          <li>🐹 Ember.js</li>
+        </ul>
+        <p>
+        I graduated from Columbia University with a BS in Computer Science and a minor in Economics. I'm passionate about improving documentation, dev tooling, and processes!
+        </p>
+      </SectionHeader>
     </div>
   );
 }
--- src/components/sections/SectionHeader/SectionHeader.jsx
+++ src/components/sections/SectionHeader/SectionHeader.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import 'components/sections/SectionHeader/SectionHeader.scss';
 
-function SectionHeader({img, imgAlt, anchor, title}) {
+function SectionHeader({children, img, imgAlt, anchor, title}) {
   return (
     <div className="SectionHeader">
       <img className="SectionHeader__Image" src={img} alt={imgAlt} />
 
-      <h2 className="SectionHeader__Title">
-        <a className="SectionHeader__TitleLink" href={`#${anchor}`}>
-          {title}
-        </a>
-      </h2>
+      <div className="SectionHeader__Content">
+        <h2 className="SectionHeader__Title">
+          <a className="SectionHeader__TitleLink" href={`#${anchor}`}>
+            {title}
+          </a>
+        </h2>
+
+        <div className="SectionHeader__Text">
+          {children}
+        </div>
+      </div>
     </div>
   );
 }
