8c5eb7c375691e8c9479467a142b6e1789918728
Quinn Torres <quinntorrs@gmail.com>
2022-08-21 13:26:41 -0400

    Add skeleton MyExperience code

---
+++ src/components/sections/MyExperience/MyExperience.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'components/sections/MyExperience/MyExperience.scss';
+
+function MyExperience() {
+  return (
+    <div className="MyExperience">
+      
+    </div>
+  );
+}
+
+export default MyExperience;
