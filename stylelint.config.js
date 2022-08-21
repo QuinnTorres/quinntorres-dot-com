c4a35726947f024c0486ce5a8db809e04f1a6931
Quinn Torres <quinntorrs@gmail.com>
2022-08-21 11:48:35 -0400

    Add rule for sass variable patterns

--- stylelint.config.js
+++ stylelint.config.js
@@ -11,6 +11,12 @@ module.exports = {
     'indentation': [
       2
     ],
+    'scss/dollar-variable-pattern': [
+      '^_([a-z][a-z0-9]*)(-[a-z0-9]+)*$',
+      {
+        'message': 'Expected variable to be kebab-case, starting with an underscore'
+      }
+    ],
     'selector-class-pattern': [
       '^[A-Z][a-zA-Z0-9]+',
       {
